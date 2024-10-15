9085e36f3b0b7bd82bd94b836d59bd07e20bcbbb
Jonathan Rico <jonathan@rico.live>
2024-10-15 23:00:48 +0200

    remove sample output from FAVORITES from script
    now it's tracked in git :)

--- commute.lisp
+++ commute.lisp
@@ -463,50 +463,6 @@
           :types '("metro")
           :lines '("5")
           :destinations '("ringen via tøyen"))))))
-; Send query: {"query":"query { stopPlace(id: \"NSR:StopPlace:58843\") { name id estimatedCalls(numberOfDepartures: 200, timeRange: 7200) { expectedDepartureTime destinationDisplay { frontText } serviceJourney { line { publicCode transportMode } } } } }"}
-; Send query: {"query":"query { stopPlace(id: \"NSR:StopPlace:58404\") { name id estimatedCalls(numberOfDepartures: 200, timeRange: 7200) { expectedDepartureTime destinationDisplay { frontText } serviceJourney { line { publicCode transportMode } } } } }"}
-; Send query: {"query":"query { stopPlace(id: \"NSR:StopPlace:58191\") { name id estimatedCalls(numberOfDepartures: 200, timeRange: 7200) { expectedDepartureTime destinationDisplay { frontText } serviceJourney { line { publicCode transportMode } } } } }"}
-;  => "
-; ---- Blommenholm -> work ----
-; 18:21 [rail] line L1 to Lillestrøm
-; 18:34 [rail] line L1 to Lillestrøm
-; 18:51 [rail] line L1 to Lillestrøm
-; 19:04 [rail] line L1 to Lillestrøm
-; 19:21 [rail] line L1 to Lillestrøm
-; 19:34 [rail] line L1 to Lillestrøm
-; 19:51 [rail] line L1 to Lillestrøm
-; 20:04 [rail] line L1 to Lillestrøm
-
-; ---- Blommenholm -> sandvika ----
-; 18:14 [rail] line L1 to Asker
-; 18:28 [rail] line L1 to Spikkestad
-; 18:44 [rail] line L1 to Asker
-; 18:58 [rail] line L1 to Spikkestad
-; 19:14 [rail] line L1 to Asker
-; 19:28 [rail] line L1 to Spikkestad
-; 19:44 [rail] line L1 to Asker
-; 19:58 [rail] line L1 to Spikkestad
-
-; ---- Nationaltheatret ----
-; 18:13 [rail] line L1 to Spikkestad
-; 18:14 [metro] line 5 to Vestli
-; 18:29 [rail] line L1 to Asker
-; 18:30 [metro] line 5 to Vestli
-; 18:43 [rail] line L1 to Spikkestad
-; 18:45 [metro] line 5 to Vestli
-; 18:59 [rail] line L1 to Asker
-; 18:59 [metro] line 5 to Vestli
-
-; ---- Hasle ----
-; 18:17 [metro] line 5 to Ringen via Tøyen
-; 18:31 [metro] line 5 to Ringen via Tøyen
-; 18:46 [metro] line 5 to Ringen via Tøyen
-; 19:01 [metro] line 5 to Ringen via Tøyen
-; 19:16 [metro] line 5 to Ringen via Tøyen
-; 19:31 [metro] line 5 to Ringen via Tøyen
-; 19:46 [metro] line 5 to Ringen via Tøyen
-; 20:01 [metro] line 5 to Ringen via Tøyen
-; "
 
 (defun handle-favorites ()
   (list 200 '(:content-type "text/plain; charset=utf-8")
